18714143b77c7b060ca420a5549d1ff788d8f37f
Richard Gong <richardgong@gmail.com>
2020-06-27 20:57:28 -0700

    install instructions

--- dir_prefs.sh
+++ dir_prefs.sh
@@ -1,3 +1,7 @@
-echo "Preferences (Options.txt, Log.txt, User Remote Scripts/MPK_gong/UserConfigurationt.txt) all live here:"
-echo "open ~/Library/Preferences/Ableton/"
+echo "[TEMPORARY] Version-specific (aka temporary) preferences (Options.txt, Log.txt, User Remote Scripts/MPK_gong/UserConfigurationt.txt) all live here:"
+echo "  open ~/Library/Preferences/Ableton/"
+echo "  ln -s ~/c/Launchpad_Pro95/user_remote_scripts/ 'User Remote Scripts'"
+echo "[PERMANENT] Python-based control scripts live here (won't be deleted upon upgrade)"
+echo "  cd '/Applications/Ableton Live 10 Suite.app/Contents/App-Resources/MIDI Remote Scripts'"
+# open "/Applications/Ableton Live 10 Suite.app/Contents/App-Resources/MIDI Remote Scripts"
 open ~/Library/Preferences/Ableton/
